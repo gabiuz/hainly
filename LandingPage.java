770b04b0635ead52450561c78ec81e10e5ddccab
gabiuz <pelagionicolai@gmail.com>
2025-05-26 01:12:52 +0800

    Revert "adjust spacing of title"
    This reverts commit 0908cea7658515ceab99bfcfa0541d6f1f3ea9eb.

--- LandingPage.java
+++ LandingPage.java
@@ -13,30 +13,22 @@ public class LandingPage extends JPanel {
     public LandingPage(Main mainFrame) {
         this.mainFrame = mainFrame;
         setLayout(new GridBagLayout());
-        titleLabel();
         initializeComponents();
-    }
-
-    private void titleLabel(){
-
-        GridBagConstraints gbcTitle = new GridBagConstraints();
-        gbcTitle.insets = new Insets(20, 5, 30, 5);
-        gbcTitle.fill = GridBagConstraints.HORIZONTAL;
-
-        // Title
-        JLabel titleLabel = new JLabel("Welcome to Hain-ly");
-        titleLabel.setFont(new Font("Roboto", Font.BOLD, 30));
-        gbcTitle.gridx = 0;
-        gbcTitle.gridy = 0;
-        gbcTitle.gridwidth = 2;
-        gbcTitle.anchor = GridBagConstraints.CENTER;
-        add(titleLabel, gbcTitle);
     }
 
     private void initializeComponents() {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5);
         gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        // Title
+        JLabel titleLabel = new JLabel("Welcome to Hain-ly");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        add(titleLabel, gbc);
 
         // Username
         gbc.gridwidth = 1;
